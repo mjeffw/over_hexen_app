9e0122ed6e6b7615a63f0f3107b0eb55a428f061
mjeffw <geek.macbeer@gmail.com>
2019-12-19 16:44:22 -0500

    added ability to upload an image file (web)

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,129 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:universal_html/prefer_universal/html.dart' as html;
 
 void main() => runApp(MyApp());
+
+class ImageModel with ChangeNotifier {
+  Uint8List _bytes;
+  Uint8List get bytes => _bytes;
+  set bytes(Uint8List bytes) {
+    _bytes = bytes;
+    notifyListeners();
+  }
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Over Hexen',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class MyHomePage extends StatelessWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient:
+            LinearGradient(colors: <Color>[Colors.grey[100], Colors.grey[400]]),
+      ),
+      child: Scaffold(backgroundColor: Colors.transparent, body: FileViewer()),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class FileViewer extends StatelessWidget {
+  const FileViewer({
+    Key key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    return ChangeNotifierProvider(
+      create: (_) => ImageModel(),
+      child: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            Header(),
+            Consumer<ImageModel>(builder: (_, image, __) => Body(image)),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Header extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black87),
+      ),
+      child: ButtonBar(
+        alignment: MainAxisAlignment.start,
+        children: <Widget>[
+          RaisedButton(
+            onPressed: () {
+              selectFile(context);
+            },
+            child: Text('Load Image'),
+            color: Colors.amber,
+          )
+        ],
+      ),
+    );
+  }
+
+  void selectFile(BuildContext context) async {
+    html.InputElement uploadInput = html.FileUploadInputElement();
+    uploadInput.click();
+
+    uploadInput.onChange.listen((e) {
+      // read file content as dataURL
+      final files = uploadInput.files;
+      if (files.length == 1) {
+        final file = files[0];
+        final reader = new html.FileReader();
+
+        reader.onLoadEnd.listen((e) {
+          _handleResult(context, reader.result);
+        });
+        reader.readAsArrayBuffer(file);
+      }
+    });
+  }
+
+  void _handleResult(BuildContext context, Uint8List result) {
+    Provider.of<ImageModel>(context, listen: false).bytes = result;
+  }
+}
+
+class Body extends StatelessWidget {
+  final ImageModel model;
+
+  Body(this.model);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 800,
+      width: 600,
+      color: Colors.blueGrey,
+      child: model.bytes != null
+          ? Image.memory(model.bytes)
+          : Container(color: Colors.transparent),
     );
   }
 }
