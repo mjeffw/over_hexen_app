8881033f1e4b5c45dbc7236757ca16ce4fdfe4f6
Wilson, Marvin (jw9615) <jw9615@att.com>
2020-03-05 09:30:19 -0500

    refactored for readability

--- lib/screens/file_viewer/widgets/header.dart
+++ lib/screens/file_viewer/widgets/header.dart
@@ -15,52 +15,63 @@ class Header extends StatelessWidget {
     final fileProvider = Provider.of<FileImageDataProvider>(context);
     // final netProvider = Provider.of<NetworkImageDataProvider>(context);
 
+    var text = Text(model.name);
+    var urlForm = _urlForm();
     return Container(
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.black87)
-      ),
+      decoration: BoxDecoration(border: Border.all(color: Colors.black87)),
       child: Row(
         // alignment: MainAxisAlignment.start,
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: RaisedButton(
-              onPressed: model.isEditingURL
-                  ? null
-                  : () => fileProvider
-                      .get((data, name) => model.updateState(name, data)),
-              child: Text('Upload Image'),
-              color: Colors.lightGreen,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: RaisedButton(
-              onPressed: null, // () => model.isEditingURL = !model.isEditingURL,
-              //netProvider.get((data, name) => model.updateState(name, data)),
-              child: model.isEditingURL ? Text('Cancel') : Text('Image URL'),
-              color: model.isEditingURL ? Colors.amber : Colors.lightGreen,
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 8.0),
-              child: IndexedStack(
-                alignment: AlignmentDirectional.centerStart,
-                children: <Widget>[
-                  Text(model.name),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 4.0),
-                    child: UrlForm(
-                        onSubmit: (data, name) =>
-                            model.updateState(name, data)),
-                  ),
-                ],
-                index: model.isEditingURL ? 1 : 0,
-              ),
-            ),
-          ),
+          _uploadImageButton(fileProvider),
+          _imageUrlButton(),
+          Expanded(child: _createIndexedStack(text, urlForm)),
         ],
+      ),
+    );
+  }
+
+  Padding _createIndexedStack(Text text, Padding urlForm) {
+    return Padding(
+        padding: EdgeInsets.symmetric(horizontal: 8, vertical: 0),
+        child: IndexedStack(
+          alignment: AlignmentDirectional.centerStart,
+          children: <Widget>[
+            text,
+            urlForm,
+          ],
+          index: model.isEditingURL ? 1 : 0,
+        ));
+  }
+
+  Padding _urlForm() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 0, vertical: 4),
+      child: UrlForm(onSubmit: (data, name) => model.updateState(name, data)),
+    );
+  }
+
+  Padding _imageUrlButton() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 0),
+      child: RaisedButton(
+        onPressed: null, // () => model.isEditingURL = !model.isEditingURL,
+        //netProvider.get((data, name) => model.updateState(name, data)),
+        child: model.isEditingURL ? Text('Cancel') : Text('Image URL'),
+        color: model.isEditingURL ? Colors.amber : Colors.lightGreen,
+      ),
+    );
+  }
+
+  Padding _uploadImageButton(FileImageDataProvider fileProvider) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+      child: RaisedButton(
+        onPressed: model.isEditingURL
+            ? null
+            : () =>
+                fileProvider.get((data, name) => model.updateState(name, data)),
+        child: Text('Upload Image'),
+        color: Colors.lightGreen,
       ),
     );
   }
